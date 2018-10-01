5f102bc993c138ee78269b822d0840b46697313c
Ian Denhardt <ian@zenhack.net>
2018-10-01 18:22:58 -0400

    More argument checking.

--- cmd/hil-vpn-privop/main.go
+++ cmd/hil-vpn-privop/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+// A regular expression matching legal vpn names.
+var vpnNameRegexp = regexp.MustCompile("^[-_a-zA-Z0-9]+$")
 
 // Print a help message to stderr and exit with the given status code.
 func usage(exitCode int) {
@@ -30,6 +35,22 @@ func checkNumArgs(count int) {
 	}
 }
 
+// Validate that `name` is a legal name for a vpn. If so, return the name,
+// otherwise exit with an error message.
+func checkVpnName(name string) string {
+	if vpnNameRegexp.MatchString(name) {
+		return name
+	}
+	fmt.Fprintf(
+		os.Stderr,
+		"Invalid vpn name %q; names may only contain dashes, underscores, "+
+			"and alphanumeric characters.\n\n",
+		name,
+	)
+	usage(1)
+	panic("unreachable")
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		usage(1)
@@ -37,15 +58,33 @@ func main() {
 	switch os.Args[1] {
 	case "create":
 		checkNumArgs(3)
+		vpnName := checkVpnName(os.Args[2])
+		vlanNo, err := strconv.ParseInt(os.Args[3], 10, 12)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing vlan number: %v\n\n", err)
+			usage(1)
+		}
+		portNo, err := strconv.ParseInt(os.Args[4], 10, 16)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing port number: %v\n\n", err)
+			usage(1)
+		}
+		// Without this the compiler complains about unused variables.
+		// We'll start using them soon:
+		_, _, _ = vpnName, vlanNo, portNo
+
 		fmt.Fprintln(os.Stderr, "Unimplemented")
 	case "start":
 		checkNumArgs(1)
+		checkVpnName(os.Args[2])
 		fmt.Fprintln(os.Stderr, "Unimplemented")
 	case "stop":
 		checkNumArgs(1)
+		checkVpnName(os.Args[2])
 		fmt.Fprintln(os.Stderr, "Unimplemented")
 	case "delete":
 		checkNumArgs(1)
+		checkVpnName(os.Args[2])
 		fmt.Fprintln(os.Stderr, "Unimplemented")
 	case "list":
 		checkNumArgs(0)
