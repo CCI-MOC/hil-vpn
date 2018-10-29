16439bdfcb691dca1b2d73c27d6503ad023796ef
Ian Denhardt <ian@zenhack.net>
2018-10-29 15:22:40 -0400

    Add some logic for allocating and freeing vpn ids/ports.

--- cmd/hil-vpnd/vpnstates.go
+++ cmd/hil-vpnd/vpnstates.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"errors"
 	"sync"
 )
+
+var ErrNoFreePorts = errors.New("There are no free OpenVPN ports")
 
 // Track the currently existent vpns, available port numbers, etc.
 type VpnStates struct {
@@ -21,4 +25,45 @@ func newStates() *VpnStates {
 		UsedPorts: map[UniqueId]uint16{},
 		FreePorts: []uint16{},
 	}
+}
+
+func (s *VpnStates) NewVpn() (UniqueId, uint16, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	var id UniqueId
+	if _, err := rand.Read(id[:]); err != nil {
+		return id, 0, err
+	}
+
+	portNo, err := s.allocPort()
+	if err == nil {
+		s.UsedPorts[id] = portNo
+	}
+	return id, portNo, err
+}
+
+func (s *VpnStates) DeleteVpn(id UniqueId) {
+	s.Lock()
+	defer s.Unlock()
+
+	portNo := s.UsedPorts[id]
+	delete(s.UsedPorts, id)
+	s.releasePort(portNo)
+}
+
+// Allocate a new port for a vpn.
+func (s *VpnStates) allocPort() (uint16, error) {
+	if len(s.FreePorts) == 0 {
+		return 0, ErrNoFreePorts
+	}
+
+	portNo := s.FreePorts[len(s.FreePorts)-1]
+	s.FreePorts = s.FreePorts[:len(s.FreePorts)-1]
+	return portNo, nil
+}
+
+// Return a port number to the free pool.
+func (s *VpnStates) releasePort(portNo uint16) {
+	s.FreePorts = append(s.FreePorts, portNo)
 }
