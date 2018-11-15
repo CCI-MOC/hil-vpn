dd01b36f853fa7d17ed97924f0f0450d524307c4
Ian Denhardt <ian@zenhack.net>
2018-11-15 18:39:42 -0500

    Use more secure cipher, and specify listen port.
    SELinux is giving us permission denied when trying to bind to an
    alternate port; we'll need to work out the right way permit this
    before deploying to a real centos system.

--- cmd/hil-vpn-privop/create.go
+++ cmd/hil-vpn-privop/create.go
@@ -5,7 +5,7 @@ package main
 // the program will exit with an error. Otherwise, the generated
 // static key is returned.
 func createCmd(vpnName string, vlanNo, portNo uint16) string {
-	cfg, err := NewOpenVpnConfig(vpnName)
+	cfg, err := NewOpenVpnConfig(vpnName, portNo)
 	chkfatal("Generating openvpn config:", err)
 	chkfatal("Saving openvpn config:", cfg.Save())
 	return cfg.Key
