145cdaa0c7686df8d5f4a088c641acaeeab85568
Ian Denhardt <ian@zenhack.net>
2018-12-04 12:20:38 -0500

    Use obmd http server setup code.
    The replace directive is because the relevant patches haven't been
    merged into obmd itself yet; I'm using that to develop locally. Once the
    obmd prs are merged we'll remove it.
    
    We also probably want to store the httpserver package somewhere more
    neutral at some point; there's no reason hil-vpn should depend on obmd.

--- cmd/hil-vpnd/main.go
+++ cmd/hil-vpnd/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"github.com/caarlos0/env"
 	"log"
 	"net/http"
+
+	"github.com/caarlos0/env"
+
+	"github.com/CCI-MOC/obmd/httpserver"
 )
 
 type config struct {
-	MinPort int `env:"MIN_VPN_PORT,required"`
-	MaxPort int `env:"MAX_VPN_PORT,required"`
+	MinPort      int `env:"MIN_VPN_PORT,required"`
+	MaxPort      int `env:"MAX_VPN_PORT,required"`
+	ServerConfig httpserver.Config
 }
 
 // Parse and validate the config, then return it.
@@ -17,6 +21,13 @@ func getConfig() config {
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatal("Parsing config from environment: ", err)
 	}
+	if err := env.Parse(&cfg.ServerConfig); err != nil {
+		log.Fatal("Parsing config from environment: ", err)
+	}
+	if err := cfg.ServerConfig.Validate(); err != nil {
+		log.Fatal(err)
+	}
+
 	if cfg.MinPort > cfg.MaxPort {
 		log.Fatal("Config error: MIN_VPN_PORT is greater than MAX_VPN_PORT")
 	}
@@ -38,5 +49,5 @@ func main() {
 	}
 
 	http.Handle("/", makeHandler(PrivOpsCmd{}, vpnStates))
-	panic(http.ListenAndServe(":8080", nil))
+	panic(httpserver.Run(&cfg.ServerConfig, nil))
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,10 @@
 module github.com/CCI-MOC/hil-vpn
 
 require (
+	github.com/CCI-MOC/obmd v0.0.0-20181108180920-2d628d0e8fc1
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/gorilla/mux v1.6.2
 	golang.org/x/sys v0.0.0-20181011152604-fa43e7bc11ba
 )
+
+replace github.com/CCI-MOC/obmd => ../obmd
